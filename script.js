51a1c4e4c0b46da982dbb8eb57d83a74fc89a7ac
tilak-c <66167345+tilak-c@users.noreply.github.com>
2025-10-16 22:12:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+const p=document.getElementById(counter);
+const incrementBtn=document.getElementById('increment');
+incrementBtn.addEventListener("click",function(){
+	let currentValue=parseInt(counterElement.textContent);
+	alert("currentValue");
+	counterElement.textContent=currentValue;
+})
