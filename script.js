0c88f152bc1afa89cf860e5600af04a6541311e9
tilak-c <66167345+tilak-c@users.noreply.github.com>
2025-10-16 22:13:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,6 +3,6 @@ const p=document.getElementById(counter);
 const incrementBtn=document.getElementById('increment');
 incrementBtn.addEventListener("click",function(){
 	let currentValue=parseInt(counterElement.textContent);
-	alert("currentValue");
-	counterElement.textContent=currentValue;
+	alert(currentValue);
+	counterElement.textContent=currentValue+1;
 })
